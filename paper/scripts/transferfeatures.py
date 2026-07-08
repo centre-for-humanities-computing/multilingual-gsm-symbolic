de84c2e91cc02da00d71b1a955d4570868a7f757
rileyherchert <160680892+rileyherchert@users.noreply.github.com>
2026-07-08 15:06:22 +0200

    improve transfer features

--- paper/scripts/transferfeatures.py
+++ paper/scripts/transferfeatures.py
@@ -350,8 +350,8 @@ def relationship_plot(
 
         handles, labels = ax.get_legend_handles_labels()
         if handles:
-            fig.legend(handles, labels, loc="lower center", ncol=len(labels), frameon=False)
-        split_out = out.with_name(f"{out.stem}_{split}{out.suffix}")
+            fig.legend(handles, labels, loc="lower center", ncol=math.ceil(len(labels) / 2), frameon=False)
+        split_out = out.with_name(f"{out.stem}_{split}{out.suffix}") if split == "original" else out
         fig.supxlabel(xlabel, y=0.08)
         fig.tight_layout(rect=(0, 0.13, 1, 1))
         fig.savefig(split_out, bbox_inches="tight")
